05cf32439d04103c086a6af8bbe2035ec6d45916
Minseok-Choi <cms05041@gmail.com>
2021-09-06 00:42:40 +0900

    add proxy and aop

--- design-pattern/src/com/company/design/Main.java
+++ design-pattern/src/com/company/design/Main.java
@@ -1,24 +1,31 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        AClazz aClazz = new AClazz();
+        //싱글톤
+        /*AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
         SocketClient aClient = aClazz.getSocketClient();
         SocketClient bClient = bClazz.getSocketClient();
 
-        /* System.out.println("두개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
-        */
-        HairDryer hairDryer = new HairDryer();
+        System.out.println("두개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));*/
+
+        //어댑터
+        /*HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
         Cleaner cleaner = new Cleaner();
@@ -27,7 +34,40 @@ public class Main {
 
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V adapter2 = new SocketAdapter(airConditioner);
-        connect(adapter2);
+        connect(adapter2);*/
+
+        //Proxy
+        /*Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+        IBrowser browser1 = new BrowserProxy("www.naver.com");
+        browser1.show();
+        browser1.show();
+        browser1.show();
+        browser1.show();*/
+
+        //AOP
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
     }
     // 콘센트
     public static void connect(Electronic110V electronic110V) {
