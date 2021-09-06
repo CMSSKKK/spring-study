a3413c5979b91ad7ad6267fabbe3abb097d46b9e
Minseok-Choi <cms05041@gmail.com>
2021-09-06 17:58:48 +0900

    add decorator

--- design-pattern/src/com/company/design/Main.java
+++ design-pattern/src/com/company/design/Main.java
@@ -2,6 +2,7 @@ package com.company.design;
 
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -50,7 +51,7 @@ public class Main {
         browser1.show();*/
 
         //AOP
-        AtomicLong start = new AtomicLong();
+        /*AtomicLong start = new AtomicLong();
         AtomicLong end = new AtomicLong();
 
         IBrowser aopBrowser = new AopBrowser("www.naver.com",
@@ -67,7 +68,20 @@ public class Main {
         System.out.println("loading time : "+end.get());
 
         aopBrowser.show();
-        System.out.println("loading time : "+end.get());
+        System.out.println("loading time : "+end.get());*/
+
+        //decorator
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        ICar audi3 = new A3(audi,"A3");
+        audi3.showPrice();
+
+        ICar audi4 = new A4(audi,"A4");
+        audi4.showPrice();
+
+        ICar audi5 = new A5(audi,"A5");
+        audi5.showPrice();
     }
     // 콘센트
     public static void connect(Electronic110V electronic110V) {
